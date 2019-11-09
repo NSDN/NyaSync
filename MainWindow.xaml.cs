9a2a1462835383eacae51f21a4b63792841dbd5f
drzzm32 <drzzm32@gmail.com>
2019-11-09 19:55:49 +0800

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NyaSync
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("logo.png"))
+            {
+                TextLogo.Visibility = Visibility.Hidden;
+                LogoBox.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "logo.png", UriKind.Absolute));
+
+                Width = SystemParameters.PrimaryScreenWidth / 3;
+                Height = Width / LogoBox.Source.Width * LogoBox.Source.Height;
+                Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+                Top = (SystemParameters.PrimaryScreenHeight - Height) / 2;
+            }
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void TextLogo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+    }
+}
---
+++ NyaSyncCLI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace NyaSyncCLI
+{
+    class Program
+    {
+        const string SERVER_ARG = "--server";
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("NyaSync CLI | usage:");
+                Console.WriteLine("use as server: " + Path.GetFileName(Environment.CommandLine) + " " + SERVER_ARG + " [config file] [index file]");
+                Console.WriteLine("use as client: " + Path.GetFileName(Environment.CommandLine) + " [server url] [target dir] [cache dir]");
+                Console.WriteLine();
+                return;
+            }
+            else
+            {
+                if (args[0] == SERVER_ARG)
+                {
+                    Console.WriteLine("NyaSync CLI | Server Side");
+                    string config = args[1];
+                    string index = args[2];
+                    NyaSyncCore.DoServerStuff(config, index);
+                }
+                else
+                {
+                    Console.WriteLine("NyaSync CLI | Client Side");
+                    string url = args[0];
+                    string target = args[1];
+                    string cache = args[2];
+                    NyaSyncCore.DoClientStuff(url, target, cache);
+                }
+            }
+        }
+    }
+}
